438f28a2955395ecb5f5f2fbe239de1fba74d6a5
Fabio Mendes <fabiobends@gmail.com>
2019-12-30 13:56:37 -0300

    first commit

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Bme-vindo to React Native!</Text>
-        <Text style={styles.instructions}><Button onPress={useState}>Clique</Button></Text>
-        <Text style={styles.instructions}><Button>Nada</Button></Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
