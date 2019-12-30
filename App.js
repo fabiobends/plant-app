a70f0c06e4aba8a907f784f82efa3cb82289dea2
Fabio Mendes <fabiobends@gmail.com>
2019-12-30 13:34:17 -0300

    Second Commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -11,8 +11,8 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}><Button onPress={useState}>Clique</Button></Text>
+        <Text style={styles.instructions}><Button>Nada</Button></Text>
       </View>
     );
   }
